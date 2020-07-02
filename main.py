3db9145778d5037314f133dfaf5eba9ea94bfbf1
happyperson10 <67008955+happyperson10@users.noreply.github.com>
2020-07-02 23:15:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import pygame
 # Init here
 pygame.init()
 # Variables here
+# Width and height of the game
 width = 800
 height = 600
+# Colors here (Red,Green,Blue)
 white = (255,255,255)
 black = (0,0,0)
+# This will make the images load
+Pawn = pygame.image.load('white-pawn-md.png')
+PawnX = 400
+pawnY = 200
+def Pawn():
+    pygame.blip(Pawn,PawnX,pawnY)
+# End of variables and function defining
 # Window of the game here
 window = pygame.display.set_mode((width,height))
+# Name of the game here
 pygame.display.set_caption('chess')
-running = False
-while not running:
+# Main loop here
+running = True
+# This will make it to for the user to close out of the game
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = True 
+            running = False
+# This is to make the screen white
     window.fill(white)
-    pygame.draw.rect(window,black,['400','300','10','100'])
+    pygame.draw.rect(window,black,[500,100,20,100])
+    Pawn()
     pygame.display.update()
+# You also need this to quit for me
 pygame.quit()
