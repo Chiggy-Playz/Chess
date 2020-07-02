4a32b39275fdacbf1b43efba2a42766ede0aeb89
happyperson10 <67008955+happyperson10@users.noreply.github.com>
2020-07-02 21:23:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ pygame.init()
 # Variables here
 width = 800
 height = 600
+white = (255,255,255)
+black = (0,0,0)
 # Window of the game here
 window = pygame.display.set_mode((width,height))
-pygame.display.set_caption("pygame")
-running = True
-while running:
+pygame.display.set_caption('chess')
+running = False
+while not running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running = True 
+    window.fill(white)
+    pygame.draw.rect(window,black,['400','300','10','100'])
+    pygame.display.update()
+pygame.quit()
