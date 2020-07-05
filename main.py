1bbbd250022156663d06b34bd03b3cab6a40c95c
happyperson10 <67008955+happyperson10@users.noreply.github.com>
2020-07-05 18:05:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,95 @@
 import pygame
-from board import Board
 # Init here
 pygame.init()
 # Variables here
-
-
-width = 800
-height = 600
+# In computers there are only 255 called RGB here it is red green blue 
+width = 1000
+height = 800
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 BLUE = (0,255,0)
 GREEN = (0,0,255)
-board = Board()
-x =  (width * 0.45)
-y = (height * 0.8)
-# Here is every piece
-Whitepawnimg = pygame.image.load('whitepawn.png')
-Whitekingimg = pygame.image.load('whiteking.png')
-Whitequeenimg = pygame.image.load('whitequeen.png')
-Whitebishopimg = pygame.image.load('whitebishop.png')
-Whiterookimg = pygame.image.load('whiterook.png')
-Whiteknightimg = pygame.image.load('whiteknight') 
-blackpawnimg = pygame.image.load('blackpawn.png')
-blackkingimg = pygame.image.load('blackking.png')
-blackqueenimg = pygame.image.load('blackqueen.png')
-blackbishopimg = pygame.image.load('blackbishop.png')
-blackrookimg = pygame.image.load('blackrook.png')
-blackknightimg = pygame.image.load('blackknight.png') 
-# Now for the board
-boardimg = pygame.image.load('board.png')
-# Window of the game here
+# Sise of the game here
 window = pygame.display.set_mode((width,height))
 # Name of the game here
 pygame.display.set_caption('Chess')
+# Putting all images here
+# This will load the image here
+Whitepawnimg = pygame.image.load('whitepawn.png')
+def whitepawn(whitepawnx,whitepawny):
+    window.blit(Whitepawnimg,(whitepawnx,whitepawny))
+whitepawnx = (width*0.2)
+whitepawny = (height*0,4)
+Whitebishopimg = pygame.image.load('whitebishop.png')
+def whitebishop(whitebishopx,whitebishopy):
+    window.blit(Whitebishopimg,(whitebishopx,whitebishopy))
+whitebishopx= (width*0.2)
+whitebishopy= (height*0,4)
+Whiteknightimg = pygame.image.load('whiteknight.png')
+def whiteknight(whiteknightx,whiteknighty):
+        window.blit(Whiteknightimg,(whiteknightx,whiteknighty))
+whiteknightx= (width*0.2)
+whiteknighty= (height*0,4)
+Whiterookimg = pygame.image.load('whiterook.png')
+def whiterook(whiterookx,whiterooky):
+        window.blit(Whiterookimg,(whiterookx,whiterooky))
+whiterookx = (width*0.2)
+whiterooky = (height*0,4)
+Whitequeenimg = pygame.image.load('whitequeen.png')
+def whitequeen(whitequeenx,whitequeeny):
+        window.blit(Whitequeenimg,(whitequeenx,whitequeeny))
+whitequeenx = (width*0.2)
+whitequeeny = (height*0,4)
+Whitekingimg = pygame.image.load('whiteking.png')
+def whiteking(whitekingx,whitekingy):
+        window.blit(Whitekingimg,(whitekingx,whitekingx))
+whitekingx = (width*0.2)
+whitekingy = (height*0,4)
+blackpawnimg = pygame.image.load('blackpawn.png')
+def blackpawn(blackpawnx,blackpawny):
+        window.blit(blackpawnimg,(blackpawnx,blackpawny))
+blackpawnx= (width*0.2)
+blackpawny = (height*0,4)
+blackbishopimg = pygame.image.load('blackbishop.png')
+def blackbishop(blackbishopx,blackbishopy):
+        window.blit(blackbishopimg,(blackbishopx,blackbishopy))
+blackpawnx= (width*0.2)
+blackpawny = (height*0,4)
+blackknightimg = pygame.image.load('blackknight.png')
+def blackknight(blackknightx,blackknighty):
+        window.blit(blackknightimg,(blackknightx,blackknighty))
+blackpawnx= (width*0.2)
+blackpawny = (height*0,4)
+
+blackrookimg = pygame.image.load('blackrook.png')
+def blackrook(blackrookx,blackrooky):
+        window.blit(blackrookimg,(blackrookx,blackrooky))
+blackrookx = (width*0.2)
+blackrookx = (height*0,4)
+blackqueenimg = pygame.image.load('blackqueen.png')
+def blackqueen(blackqueenx,blackqueeny):
+        window.blit(blackqueenimg,(blackqueenx,blackqueeny))
+blackqueenx = (width*0.2)
+blackqueeny = (height*0,4)
+blackkingimg = pygame.image.load('blackking.png')
+def blackking(blackkingx,blackkingy):
+        window.blit(blackkingimg,(blackkingx,blackkingy))
+blackkingx = (width*0.8)
+blackkingy = (height*0.4)
+# Now for the board
+boardimg = pygame.image.load('board.png')
+def board(boardx,boardy):
+    window.blit(boardimg,(boardx,boardy))
+boardx = (width * 0,2)
+boardy = (height * 0,4)
 # Main loop here
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    board.draw_tiles(window)
-    
-    img_rect = img.get_rect()
-    window.blit(img,(0,0))
-    # pygame.draw.rect(window,(255,0,0),[0,0,400,400])
+    board(boardx,boardy)
     pygame.display.update()
+pygame.quit()
+quit()
